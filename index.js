23f584f50a000ba5b73620e703c309bc5900f560
Ryan Karumanchery <ryankarumanchery@gmail.com>
2020-07-12 20:45:25 -0400

    Pacman and BN datasets working

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const nodeFetch = require("node-fetch");
 const fs = require("fs");
 
-// Page 2 for search_url
-//search_url =
-// ("https://www.googleapis.com/customsearch/v1?key=AIzaSyBJFEqcGUuwrai9n0xedh3CTDaFQK53zUw&cx=009315961641472818372:kyqr5g9u5wc&q=pac-man&start=11");
+const key = "AIzaSyBJFEqcGUuwrai9n0xedh3CTDaFQK53zUw";
+const alt_key = "AIzaSyA8xWMzI9lmgl3zc4JxB_kzPopuOa0O5ak";
+const pacman_urls = [
+  `https://www.googleapis.com/customsearch/v1?key=${key}&cx=009315961641472818372:kyqr5g9u5wc&q=pac-man`,
+  `https://www.googleapis.com/customsearch/v1?key=${key}&cx=009315961641472818372:kyqr5g9u5wc&q=pac-man&start=11`,
+  `https://www.googleapis.com/customsearch/v1?key=${key}&cx=009315961641472818372:kyqr5g9u5wc&q=pac-man&start=21`,
+];
 
-const search_url_1 =
-  "https://www.googleapis.com/customsearch/v1?key=AIzaSyA8xWMzI9lmgl3zc4JxB_kzPopuOa0O5ak&cx=009315961641472818372:kyqr5g9u5wc&q=pac-man";
+const bn_urls = [
+  `https://www.googleapis.com/customsearch/v1?key=${key}&cx=009315961641472818372:kyqr5g9u5wc&q=bandai+namco`,
+  `https://www.googleapis.com/customsearch/v1?key=${key}&cx=009315961641472818372:kyqr5g9u5wc&q=bandai+namco&start=11`,
+  `https://www.googleapis.com/customsearch/v1?key=${key}&cx=009315961641472818372:kyqr5g9u5wc&q=bandai+namco&start=21`,
+];
 
-const search_url_2 =
-  "https://www.googleapis.com/customsearch/v1?key=AIzaSyA8xWMzI9lmgl3zc4JxB_kzPopuOa0O5ak&cx=009315961641472818372:kyqr5g9u5wc&q=bandai+namco";
-
-const search_url_3 =
-  "https://www.googleapis.com/customsearch/v1?key=AIzaSyA8xWMzI9lmgl3zc4JxB_kzPopuOa0O5ak&cx=009315961641472818372:kyqr5g9u5wc&q=videogames";
+const videogames_urls = [
+  `https://www.googleapis.com/customsearch/v1?key=${key}&cx=009315961641472818372:kyqr5g9u5wc&q=videogames`,
+  `https://www.googleapis.com/customsearch/v1?key=${key}&cx=009315961641472818372:kyqr5g9u5wc&q=videogames&start=11`,
+  `https://www.googleapis.com/customsearch/v1?key=${key}&cx=009315961641472818372:kyqr5g9u5wc&q=videogames&start=21`,
+];
 
 /**
- *
  * Returns True if at least 200 words
+ * @param {String} str - string to check
  */
 function WordCount200(str) {
   if (str === undefined || str.length == 0) {
@@ -31,7 +38,12 @@ function WordCount200(str) {
  * @param {string} url - link to be filtered
  */
 async function url_valid(url) {
-  const invalid_urls = ["youtube", "apps.apple", "play.google"];
+  const invalid_urls = [
+    "youtube",
+    "apps.apple",
+    "play.google",
+    "login.bandainamco",
+  ];
   var valid = true;
   for (let x = 0; x < invalid_urls.length; x++) {
     if (url.includes(invalid_urls[x])) {
@@ -46,6 +58,7 @@ async function url_valid(url) {
  * @param {string} url - Url to extract links from
  */
 async function get_links(url) {
+  console.log("searching for links");
   try {
     let links = [];
     let linkcount = 10;
@@ -53,11 +66,6 @@ async function get_links(url) {
     // Get data
     const response = await nodeFetch(url);
     const json = await response.json();
-
-    for (let x = 0; x < 10; x++) {
-      console.log(json.items[x].title);
-    }
-
     // Push data items to list
     for (let x = 0; x < 10; x++) {
       let item = {};
@@ -69,14 +77,14 @@ async function get_links(url) {
       //Add valid items to list
       const valid = await url_valid(item.link);
       if (typeof item.title !== undefined && valid) {
-        console.log(`${item.link} pushed`);
+        //console.log(`${item.link} pushed`);
         links.push(item);
       } else {
-        console.log(`${item.link} not pushed, linkcount reduced`);
+        //console.log(`${item.link} not pushed, linkcount reduced`);
         linkcount--;
       }
     }
-    console.log(`Linkcount is ${linkcount}`);
+    //console.log(`${linkcount} valid links collected`);
 
     let dataset = [];
     // Push list items to dataset
@@ -94,6 +102,7 @@ async function get_links(url) {
   } catch (error) {
     console.log(error);
   }
+  console.log("Done searching for links");
 }
 
 /**
@@ -102,65 +111,61 @@ async function get_links(url) {
  */
 async function parse_site(url) {
   try {
-    //nodeFetch(url).then(res => res.text()).then(body=>console.log(body))
     let body = await nodeFetch(url).then((res) => res.text());
     return body;
-    //console.log(response);
   } catch (error) {
     console.log(error);
-    return "Error Here";
+    return "Error parsing site";
   }
+}
+
+async function crawl(url_arr, name) {
+  console.log(`Crawling ${name} Google Search Results ...`);
+  let dataset = [];
+  let max_length = url_arr.length * 10;
+  let file_count = 0;
+  for (let i = 0; i < url_arr.length; i++) {
+    let new_data = await get_links(url_arr[i]);
+    dataset = dataset.concat(new_data);
+  }
+
+  console.log(`${dataset.length} valid "${name}" sites`);
+
+  for (let x = 0; x < max_length; x++) {
+    let name_str = "./Dataset/" + name + "/article" + x;
+
+    if (dataset[x] !== undefined) {
+      try {
+        fs.writeFile(name_str, dataset[x].content, function (err) {
+          if (err) throw err;
+          console.log(`File 'article${x}' saved successfully!`);
+        });
+        file_count++;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  return file_count;
 }
 
 /**
  * Driver Function
  */
 async function main() {
-  // Get Data Sets
-  let dataset = await get_links(search_url_1);
-  let dataset2 = await get_links(search_url_2);
-  let dataset3 = await get_links(search_url_3);
-
-  for (let x = 0; x < 10; x++) {
-    // Standardize article name
-    let name_str1 = "./Dataset/Pacman/article" + x;
-
-    // Write to files
-    if (dataset[x] !== undefined) {
-      fs.writeFile(name_str1, dataset[x].content, function (err) {
-        if (err) throw err;
-        console.log("File saved!");
-      });
-    }
-  }
-
-  for (let x = 0; x < 10; x++) {
-    let name_str2 = "./Dataset/BandaiNamco/article" + x;
-
-    if (dataset2[x] !== undefined) {
-      fs.writeFile(name_str2, dataset2[x].content, function (err) {
-        if (err) throw err;
-        console.log("File saved!");
-      });
-    }
-  }
-
-  for (let x = 0; x < 10; x++) {
-    let name_str3 = "./Dataset/VideoGames/article" + x;
-    if (dataset3[x] !== undefined) {
-      fs.writeFile(name_str3, dataset3[x].content, function (err) {
-        if (err) throw err;
-        console.log("File saved!");
-      });
-    }
-  }
-
-  return await dataset;
-  // console.log("Length of data is " + dataset.length);
+  // Crawl Sites
+  crawl(pacman_urls, "Pacman").then((count) => {
+    console.log(`${count} links saved!`);
+  });
+  crawl(bn_urls, "BandaiNamco").then((count) => {
+    console.log(`${count} links saved!`);
+  });
+  crawl(videogames_urls, "VideoGames").then((count) => {
+    console.log(`${count} links saved!`);
+  });
 }
 
-/* Driver code */
-// Make directories
+// Run Code
 fs.mkdir("/Dataset/Pacman", { recursive: true }, (err) => {
   if (err) throw err;
 });
@@ -170,5 +175,4 @@ fs.mkdir("/Dataset/BandaiNamco", { recursive: true }, (err) => {
 fs.mkdir("/Dataset/VideoGames", { recursive: true }, (err) => {
   if (err) throw err;
 });
-// Run Code
 main();
